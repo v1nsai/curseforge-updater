04e2fca97758f41a4897f1d9f1d435aa41de1460
Dr Ew <doctor_ew@protonmail.com>
2026-01-26 16:54:05 +0000

    smarter local dev scripts

--- scripts/build_push.sh
+++ scripts/build_push.sh
@@ -11,4 +11,10 @@ fi
 
 docker compose build --no-cache
 docker tag doctor3w/curseforge-updater:latest doctor3w/curseforge-updater:"$APP_VERSION"
+read -p "Do you want to push the image doctor3w/curseforge-updater:$APP_VERSION to Docker Hub? (y/N) " choice
+if [[ "$choice" != "y" && "$choice" != "Y" ]]; then
+    echo "Image not pushed."
+    exit 0
+fi
 docker push doctor3w/curseforge-updater:"$APP_VERSION"
+docker push doctor3w/curseforge-updater:latest
--- scripts/clean_build.sh
+++ scripts/clean_build.sh
@@ -2,7 +2,14 @@
 
 set -e
 
+# check if $.services.curseforge-updater.build is defined and not commented out in docker-compose.yaml
+if ! grep -q '^\s*#\s*build:' docker-compose.yaml; then
+    echo "Building locally from Dockerfile..."
+else
+    echo "You probably want to uncomment the build section in docker-compose.yaml before running this script..."
+    exit 1
+fi
 docker compose down
 docker compose build --no-cache
 docker compose up -d --force-recreate
-docker compose logs -f
+docker compose logs -f --timestamps
